7d019ecf6726c2e7cffafb7c4657f9641b15a112
RC <221199316+RC-ia@users.noreply.github.com>
2026-09-25 20:23:29 +0000

    Add Site Aleatorio Tech marketplace and plugin architecture

---
+++ plugins/manifest.js
@@ -0,0 +1,6 @@
+window.SITE_ALEATORIO_PLUGIN_MANIFEST = [
+  './plugins/hardware.plugin.js',
+  './plugins/peripherals.plugin.js',
+  './plugins/software.plugin.js',
+  './plugins/other.plugin.js',
+];
