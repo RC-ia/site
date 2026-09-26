ed70480c865a34b9aa21af497af7e3fb084250ac
middes0 <kalewoliveira04@gmail.com>
2026-09-25 22:45:36 -0500

    Use CentralMarket storefront catalog

--- plugins/manifest.js
+++ plugins/manifest.js
@@ -1,6 +1,3 @@
 window.SITE_ALEATORIO_PLUGIN_MANIFEST = [
-  './plugins/hardware.plugin.js',
-  './plugins/peripherals.plugin.js',
-  './plugins/software.plugin.js',
-  './plugins/other.plugin.js',
+  './plugins/storefront.plugin.js'
 ];
